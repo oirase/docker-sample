e75522c7bee4c69680d72a41268dc1289bceac1d
oirase <cashewnutmuffin@outlook.com>
2020-11-25 13:56:29 +0900

    heroku

---
+++ heroku/docker_compose_heroku/webapp/RedisClass.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const redis = require('redis')
+
+module.exports = class Redis {
+  constructor() {
+    this.redis = redis.createClient()
+    this.redis.on('error', function (err) {
+      throw err
+    })
+  }
+
+  set setData (data) {
+    const client = this.redis
+
+    client.exists(data.key, function (err, reply) {
+      if (reply !== 1) {
+        client.hmset(data.key, data.value, function(err, res) {
+          console.log('redis set succcess')
+          client.quit()
+        })
+      } else {
+         console.log('redis key exists')
+         client.quit()
+      }
+    })
+  }
+
+  get getData () {
+    const client = this.redis
+    client.hgetall('frameworks', function(err, result) {
+      console.log('redis get', result)
+      client.quit()
+    })
+  }
+}
