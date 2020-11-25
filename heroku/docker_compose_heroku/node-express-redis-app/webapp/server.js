cc2f1cb3e160c85d65cc133ed589d0a347e79f17
oirase <cashewnutmuffin@outlook.com>
2020-11-25 17:36:38 +0900

    heroku

---
+++ heroku/docker_compose_heroku/node-express-redis-app/webapp/server.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const http = require('http')
+const express = require('express')
+const app = express()
+const RedisClass = require('./RedisClass.js')
+
+app.use('/', (req, res) => {
+  const Redis = new RedisClass()
+  Redis.setData({
+    key: 'frameworks',
+    value: {
+      'javascript': 'AngularJS',
+      'css': 'Bootstrap',
+      'node': 'Express'
+    }
+  })
+  const result = Redis.getData()
+  console.log(result)
+  res.send("node hello")
+})
+app.listen(8080, () => {
+  console.log('Server listen');
+})
---
+++ heroku/docker_compose_heroku/php-test/myapp/public/index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>docker sample</title>
+</head>
+<body>
+  <p>docker sample php works</p>
+</body>
+</html>
